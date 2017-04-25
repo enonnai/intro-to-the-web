c3bee5004a021561d9e885f6abcfb74cd125a7df
Cristina Iannone <iannonecristina@gmail.com>
2017-04-25 13:01:54 +0100

    Adds cat_form.erb and POST method

--- lib/app.rb
+++ lib/app.rb
@@ -1,12 +1,23 @@
 require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "hello"
+end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Shady", "Viking"].sample
   erb(:index)
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
 end
+
+set :session_secret, 'super secret'
